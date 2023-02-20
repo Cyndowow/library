91811f9aa22481905644e69741c0536f37d801af
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-02-20 11:20:39 +0100

    Add function to display books

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 class Book {
     constructor(
         title, 
@@ -13,12 +11,24 @@ class Book {
     }
 }
 
+class Library {
+    constructor() {
+      this.books = []
+    }
+  
+    addBook(newBook) {
+        this.books.push(newBook)
+    }
+}
+
+const myLibrary = new Library();
+
 function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(this.title, this.author, this.pages, this.read);
+    myLibrary.push(title, author, pages, read);
 }
 
 function displayMyLibrary() {
-    myLibrary.forEach
+    
 }
 
 const createBookCard = (book) => {
@@ -32,15 +42,59 @@ const createBookCard = (book) => {
 
     title.textContent = `${book.title}`
     author.textContent = `${book.author}`
-    pages.textContent = `${book.pages}`
+    pages.textContent = `${book.pages} pages`
 
     if (book.read == true) {
         read.textContent = "Read"
     } else {
         read.textContent = "Not read"
     }
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+    booksContainer.appendChild(bookCard);
 }
 
 const addBookBtn = document.getElementById('addBook');
 const addBookModal = document.getElementById('addBookModal');
 const addBookForm = document.getElementById('addBookForm');
+const booksContainer = document.getElementById('booksContainer');
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').checked
+    return new Book(title, author, pages, read)
+}
+
+const addBook = (e) => {
+    e.preventDefault();
+    const newBook = getBookFromInput();
+
+    myLibrary.addBook(newBook);
+    updateBooksContainer();
+    resetBookForm();
+}
+
+const resetBookForm = () => {
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('pages').value = "";
+    read = document.getElementById('read').checked = false
+}
+
+addBookForm.onsubmit = addBook;
+
+const resetBooksContainer = () => {
+    booksContainer.innerHTML = '';
+}
+
+const updateBooksContainer = () => {
+    resetBooksContainer();
+    for(let book of myLibrary.books) {
+        createBookCard(book);
+    }
+}
